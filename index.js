0e6cea493b81f0fb8edd5384c345a8a7d247ae4e
Petter Sandholdt <petter@sandholdt.se>
2015-02-03 22:27:08 +0100

    Fix bad code

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ var start = false;
 function grantClient() {
 	spotifyApi.clientCredentialsGrant()
   	.then(function(data) {
-        console.log('Got new access token, valid for', data['expires_in'], 'seconds');
-	    spotifyApi.setAccessToken(data['access_token']);
+        console.log('Got new access token, valid for', data.expires_in, 'seconds');
+	    spotifyApi.setAccessToken(data.access_token);
 	    start = true;
-	    setTimeout(grantClient, data['expires_in']*1000);
+	    setTimeout(grantClient, data.expires_in*1000);
 	  }, function(err) {
 	        console.log('Something went wrong when retrieving an access token', err);
 	        process.exit(1);
@@ -29,34 +29,37 @@ var fetchPlaylist = function() {
 	var writeLastDate;
 	if (process.env.REDISTOGO_URL) {
 		var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-		var client = redis.createClient(rtg.port, rtg.hostname);
+		client = redis.createClient(rtg.port, rtg.hostname);
 		client.auth(rtg.auth.split(":")[1]);
 		client.on("error", function (err) {
         	console.log("Redis - Error " + err);
     	});
 		client.get("lastDate", function(err, value) {
-			if (!err)
+			if (!err) {
 				lastDate = new Date(value);
-		})
+			}
+		});
 		writeLastDate = function(date) {
 			client.set('lastDate', date);
 		};
 	} else {
 		lastDate = new Date(fs.readFileSync('./last_date.txt').toString() );
 		writeLastDate = function(date) {
-			fs.writeFile("./last_date.txt", lastDate, function() {});
-		}
+			fs.writeFile("./last_date.txt", date, function() {});
+		};
 
 	}
 
 	return function() {
-		if (!start) return;
+		if (!start) {
+			return;
+		}
 		console.log("Last fetched at:", lastDate);
 		spotifyApi.getPlaylist(spotifyUser, spotifyPlaylistId, {fields: 'tracks.items(added_by.id,added_at,track(name,artists.name,album.name)),name,external_urls.spotify'})
 		  .then(function(data) {
 		    for (var i in data.tracks.items) {
 		   	  var date = new Date(data.tracks.items[i].added_at);
-		   	  if((lastDate == undefined) || (date > lastDate)) {
+		   	  if((lastDate === undefined) || (date > lastDate)) {
 		   	  	post(data.name, 
 		   	  		data.external_urls.spotify, 
 		   	  		data.tracks.items[i].added_by ? data.tracks.items[i].added_by.id : "Unknown",
@@ -69,7 +72,7 @@ var fetchPlaylist = function() {
 		  }, function(err) {
 		    console.log('Something went wrong!', err);
 		  });
-	}
+	};
 };
 
 slack.onError = function (err) {
